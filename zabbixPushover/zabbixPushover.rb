e2beb815f96b4c4bd97479e53a94652072468ac4
Alexander Weleczka <A.Weleczka@schokopro.com>
2017-03-18 09:55:26 +0100

    Fixed codequality-issues

--- zabbixPushover/zabbixPushover.rb
+++ zabbixPushover/zabbixPushover.rb
@@ -1,82 +1,119 @@
 #!/usr/bin/env ruby
+# frozen_string_literal: true
 
 require 'uri'
 require 'net/http'
 
 # Information related to Pushover-API
-pushoverhost	= "https://api.pushover.net/1/messages.json"
-pushovertoken	= ""
+pushoverhost  = 'https://api.pushover.net/1/messages.json'
+pushovertoken = ''
 
 # Information related to your Zabbix-Server
-zabbixhost	= "http://127.0.0.1/zabbix"
+zabbixhost = 'http://127.0.0.1/zabbix'
 
 ###
 
 usertoken, payload = ARGV
-unless usertoken.nil? or payload.nil?
-  zabbixdata = Hash.new
-  payload.split("\n").each { |load|
-    tmp = load.split("=")
-    zabbixdata[tmp[0].strip] = tmp[1].strip
-  }
-  unless zabbixdata["TRIGGER.STATUS"].nil? && zabbixdata["TRIGGER.SEVERITY"].nil?
-    subject 	= ""
-    message 	= ""
-    priority	= ""
-    retrie      = ""
-    expire      = ""
-    eventurl	= zabbixhost + "/tr_events.php?triggerid=" + zabbixdata["TRIGGER.ID"] + "&eventid=" + zabbixdata["EVENT.ID"]
-    if zabbixdata["TRIGGER.STATUS"] == "OK"
-      subject = zabbixdata["HOST.NAME1"] + " recovered"
-      message = zabbixdata["ITEM.NAME1"] + " = " + zabbixdata["ITEM.VALUE1"]
-      case zabbixdata["TRIGGER.SEVERITY"]
-      when "Information", "Warning"
-        priority = "-2"
-      else
-        priority = "-1"
-      end
-    else
-      subject = zabbixdata["TRIGGER.SEVERITY"] + " : " + zabbixdata["HOST.NAME1"] + " " + zabbixdata["TRIGGER.NAME"]
-      message = zabbixdata["ITEM.NAME1"] + " = " + zabbixdata["ITEM.VALUE1"]
-      case zabbixdata["TRIGGER.SEVERITY"]
-      when "Information"
-        priority = "-2"
-      when "Warning"
-        priority = "-1"
-      when "Average"
-        priority = "0"
-      when "High"
-        priority = "1"
-      when "Disaster"
-        priority = "2"
-        retrie = "60"
-        expire = "900"
-      end
-    end
-
-    uri = URI(pushoverhost)
-    req = Net::HTTP::Post.new(uri.path)
-    req.set_form_data(
-      'token' => pushovertoken,
-      'user' => usertoken,
-      'html' => 1,
-      'title' => subject,
-      'message' => message,
-      'url' => eventurl,
-      'url_title' => "Open in Zabbix",
-      'priority' => priority,
-      'retry' => retrie,
-      'expire' => expire
-    )
-    req["Content-Type"] = "application/x-www-form-urlencoded"
-
-    res = Net::HTTP.start(uri.hostname, uri.port, :use_ssl => uri.scheme == 'https') do |http|
-      http.request(req)
-    end
-
-    puts res.body
-  end
-else
-  puts "Usage: ./zabbixPushover.rb <user_api_token> <zabbix_payload>"
+unless usertoken.nil? || payload.nil?
+  puts 'Usage: ./zabbixPushover.rb <pushover_user_token> <zabbix_payload>'
+  exit 1
 end
 
+zabbixdata = {}
+payload.split("\n").each do |load|
+  tmp = load.split('=')
+  zabbixdata[tmp[0].strip] = tmp[1].strip
+end
+
+unless zabbixdata['TRIGGER.STATUS'].nil? || zabbixdata['TRIGGER.SEVERITY'].nil?
+  puts 'Payload is missing required data'
+  exit 1
+end
+
+subject  = ''
+message  = ''
+priority = '0'
+retrie   = ''
+expire   = ''
+eventurl = [
+  zabbixhost,
+  '/tr_events.php?triggerid=',
+  read_from_hash(zabbixdata, 'TRIGGER.ID'),
+  '&eventid=',
+  read_from_hash(zabbixdata, 'EVENT.ID')
+].join
+
+if zabbixdata['TRIGGER.STATUS'] == 'OK'
+  subject = [
+    read_from_hash(zabbixdata, 'HOST.NAME1'),
+    ' recovered'
+  ].join
+  message = [
+    read_from_hash(zabbixdata, 'ITEM.NAME1'),
+    ' = ',
+    read_from_hash(zabbixdata, 'ITEM.VALUE1')
+  ].join
+  priority = case zabbixdata['TRIGGER.SEVERITY']
+             when 'Information', 'Warning'
+               '-2'
+             else
+               '-1'
+             end
+else
+  subject = [
+    read_from_hash(zabbixdata, 'TRIGGER.SEVERITY'),
+    ' : ',
+    read_from_hash(zabbixdata, 'HOST.NAME1'),
+    ' ',
+    read_from_hash(zabbixdata, 'TRIGGER.NAME')
+  ].join
+  message = [
+    read_from_hash(zabbixdata, 'ITEM.NAME1'),
+    ' = ',
+    read_from_hash(zabbixdata, 'ITEM.VALUE1')
+  ].join
+  priority = case zabbixdata['TRIGGER.SEVERITY']
+             when 'Information'
+               '-2'
+             when 'Warning'
+               '-1'
+             when 'Average'
+               '0'
+             when 'High'
+               '1'
+             when 'Disaster'
+               '2'
+             end
+  if zabbixdata['TRIGGER.SEVERITY'] == 'Disaster'
+    retrie = '60'
+    expire = '900'
+  end
+end
+
+uri = URI(pushoverhost)
+req = Net::HTTP::Post.new(uri.path)
+req.set_form_data(
+  'token' => pushovertoken,
+  'user' => usertoken,
+  'html' => 1,
+  'title' => subject,
+  'message' => message,
+  'url' => eventurl,
+  'url_title' => 'Open in Zabbix',
+  'priority' => priority,
+  'retry' => retrie,
+  'expire' => expire
+)
+
+puts Net::HTTP.start(uri.hostname, uri.port, use_ssl: true) do |http|
+  http.request(req)
+end
+exit 0
+
+def read_from_hash(hash, key)
+  if hash[key].nil?
+    '{' + key + '}'
+  else
+    hash[key]
+  end
+end
